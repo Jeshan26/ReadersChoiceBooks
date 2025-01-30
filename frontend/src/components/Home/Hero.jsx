f0d86a6235c24076f574767ce240a31e112c1180
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2025-01-30 13:49:10 -0500

    home pg without featured books

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
 const Hero = () => {
-  return <div className='h-[77.5vh]'>
-    <div></div>
-    dic
-  </div>
+  return (
+    <div className='h-[77.5vh] flex'>
+      <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
+        <h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>
+          Uncover Your Next Page-Turner
+        </h1>
+        <p className='mt-4 text-xl text-zinc-300 text-center lg:text-left'>
+          Unleash your next adventure with books from every genre. From thrilling mysteries to heartwarming romances and sci-fi, discover your next unforgettable story today.
+        </p>
+        <div className='mt-8'>
+          <button className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
+            Explore Books
+          </button>
+        </div>
+      </div>
+      <div className='w-full lg:w-3/6 h-auto lg:h-[100%] flex items-center justify-center'>
+        <img src="./hero_t.png" alt="hero" />
+      </div>
+    </div>
+  );
 };
 
 export default Hero
