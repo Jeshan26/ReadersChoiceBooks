dc860a8d41d5514f10f1f235b896a3b9804dce7c
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2025-02-04 06:48:31 -0500

    Trying to connect with backend but error in the axios

--- frontend/src/components/Home/RecentlyAdded.jsx
+++ frontend/src/components/Home/RecentlyAdded.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 
 const RecentlyAdded = () => {
+    const[Data, setData] = useState();
+    useEffect(() => {
+        const fetch = async () => {
+            const response = await axios.get('http://localhost:1000/api/v1/get-recent-books');
+            console.log(response);
+        };
+        fetch();
+    }, []);
   return (
   <div className='mt-8 px-4'>
     <h4 className='text-3xl text-yellow-100'>Recently added books</h4>
