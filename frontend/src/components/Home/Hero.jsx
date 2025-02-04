b3ce07210f9271b618a4e7a963a14c5bb1d4aa01
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2025-02-04 06:35:29 -0500

    added recently added-books and update the navbar

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className='h-[77.5vh] flex'>
-      <div className='w-full lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
+    <div className='h-[77.5vh] flex flex-col md:flex-row items-center justify-center'>
+      <div className='w-full mb-12 md:mb-0 lg:w-3/6 flex flex-col items-center lg:items-start justify-center'>
         <h1 className='text-4xl lg:text-6xl font-semibold text-yellow-100 text-center lg:text-left'>
           Uncover Your Next Page-Turner
         </h1>
@@ -11,9 +12,9 @@ const Hero = () => {
           Unleash your next adventure with books from every genre. From thrilling mysteries to heartwarming romances and sci-fi, discover your next unforgettable story today.
         </p>
         <div className='mt-8'>
-          <button className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
+          <Link to="/all-books" className='text-yellow-100 text-xl lg:text-2xl font-semibold border border-yellow-100 px-10 py-3 hover:bg-zinc-800 rounded-full'>
             Explore Books
-          </button>
+          </Link>
         </div>
       </div>
       <div className='w-full lg:w-3/6 h-auto lg:h-[100%] flex items-center justify-center'>
