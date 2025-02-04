1b29559ae80ebf281a623571ef29ea488188f32a
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2025-02-04 06:52:15 -0500

    install cors in the backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
 require('./config/connection');
 const user = require('./routes/users');
