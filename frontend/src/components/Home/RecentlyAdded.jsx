893b08f2d263f4faa4a8d892581d670cfb636e3b
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-02-12 11:14:41 -0500

    Added Bookcard template and did minor changes in recently added books

--- frontend/src/components/Home/RecentlyAdded.jsx
+++ frontend/src/components/Home/RecentlyAdded.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import axios from 'axios';
+import BookCard from '../BookCard/BookCard';
+import Loader from '../Loader/Loader';
 
 const RecentlyAdded = () => {
     const[Data, setData] = useState();
     useEffect(() => {
         const fetch = async () => {
+          // The backend axios was accepting authorization for below URI 
+          // but this should be public URI so i made this below api a public api(backend) instead of authorized because this is visible at home page without any kind of authorization.
             const response = await axios.get('http://localhost:1000/api/v1/get-recent-books');
-            setData(response.data.data);
+            setData(response.data.books);
         };
         fetch();
     }, []);
   return (
   <div className='mt-8 px-4'>
     <h4 className='text-3xl text-yellow-100'>Recently added books</h4>
-    <div className='my-4 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4'>
-      {Data && Data.map((item,i) =><div key={i}>
-        <BookCard data={items} />{''}
+    {!Data && (
+      <div className='flex items-center justify-center my-8'>
+        <Loader /> {" "}
+      </div>
+    )}
+    <div className='my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4'>
+    
+      {Data && Data.map((items,i) => 
+      <div key={i}>
+        <BookCard data={items} />{' '}
       </div>)}
     </div>
   </div>
