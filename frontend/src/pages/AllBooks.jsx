6a28c1ec93e51a26434d70c64c09aeca6c91a605
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-02-12 11:28:11 -0500

    Updated - Fetch books from backend api to show them on allbooks section

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,  { useEffect,useState } from 'react'
+import axios from "axios";
+
+import Loader from '../components/Loader/Loader'
+import BookCard from '../components/BookCard/BookCard'
 
 function AllBooks() {
+
+  const[Data, setData] = useState();
+  useEffect(() => {
+      const fetch = async () => {
+        // The backend axios was accepting authorization for below URI 
+        // but this should be public URI so i made this below api a public api(backend) instead of authorized because this is visible at home page without any kind of authorization.
+          const response = await axios.get('http://localhost:1000/api/v1/get-all-books');
+          setData(response.data.books);
+      };
+      fetch();
+  }, []);
+
   return (
-    <div>AllBooks</div>
+    <div className='bg-zinc-900 h-auto px-12 py-8'>
+      <h4 className='text-3xl text-yellow-100'>All books</h4>
+    {!Data && (
+      <div className='flex items-center justify-center my-8'>
+        <Loader /> {" "}
+      </div>
+    )}
+    <div className='my-8 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-4'>
+    
+      {Data && Data.map((items,i) => 
+      <div key={i}>
+        <BookCard data={items} />{' '}
+      </div>)}
+    </div>
+    </div>
   )
 }
 
