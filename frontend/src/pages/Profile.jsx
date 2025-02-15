1d76402c45e1635be660943b8758e8c8df13001e
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-02-14 22:59:36 -0500

    Implemeted remove book from favorite functionality

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -24,11 +24,11 @@ const Profile = () => {
   },[])
 
   return (
-    <div className='bg-zinc-900 px-2 md:px-12 py-8 flex flex-row md-flex-col h-screen gap-4 text-white'>
+    <div className='bg-zinc-900 px-2 md:px-12 py-8 flex flex-row md-flex-col  gap-4 text-white'>
 
       {!Profile && <div className='w-full h-[100%] flex items-center justify-center'> <Loader /> </div>}
       {Profile && <>
-        <div className='w-full md:w-1/6 '>
+        <div className='w-full md:w-1/6 h-screen '>
           <Sidebar data = {Profile}/>
         </div>
         <div className='w-full md:w-5/6'>
