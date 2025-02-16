6cea6545d80bf09a844e9465073ecf138ac20c26
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-02-16 13:24:22 -0500

    Implemented cart functionality - placing order

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Loader from "../components/Loader/Loader";
 import axios from "axios";
 import { AiFillDelete } from "react-icons/ai";
+import {useNavigate} from "react-router-dom";
+
 
 
 function Cart() {
 
+  const navigate = useNavigate();
   const [ Cart, setCart ] = useState();
   const [ Total, setTotal ] = useState(0);
   const headers = {
@@ -31,20 +34,29 @@ function Cart() {
     alert(response.data.message);
   };
 
+  const PlaceOrder = async ()=>{
+    try{
+    const response = await axios.post("http://localhost:1000/api/v1/place-order",
+      {order : Cart},
+      { headers }
+    );
+    alert(response.data.message);
+    navigate("/profile/orderHistry");
+  }catch(error){
+    console.log(error);
+  }
+  };
+
   useEffect(() => {
     if(Cart && Cart.length > 0 ){
-      let total = 0;
-      Cart.map((items) => {
-        total += items.price;
-      });
+      let total =  Cart.reduce((sum,items) => sum + items.price,0);
       setTotal(total);
-      total = 0;
     }
   },[Cart]);
   
   return (
     <div className='bg-zinc-900 px-12 h-screen py-8'>
-      {!Cart && <Loader />}
+      {!Cart && <div className='w-full h-[100%] flex items-center justify-center'> <Loader /></div>}
       {Cart && Cart.length === 0 && (
         <div className='h-screen'>
           <div className='h-[100%] flex items-center justify-center flex-col'>
@@ -104,6 +116,25 @@ function Cart() {
             </div>
           ))}
         </> 
+      )}
+      {Cart && Cart.length > 0 && (
+        <div className='mt-4 w-full flex items-center justify-end'>
+          <div className='p-4 bg-zinc-800 rounded'>
+            <h1 className='text-3xl text-zinc-200 font-semibold'>
+              Total Amount
+            </h1>
+            <div className='mt-3 flex items-center justify-between text-xl text-zinc-200'>
+              <h2>{Cart.length} books</h2><h2>$ { Total }</h2>
+            </div>
+            <div className='w-[100%] mt-3'>
+              <button className='bg-zinc-100 rounded px-4 py-2 flex justify-center w-full font-semibold hover:bg-zinc-400 transition-all duration-300'
+                onClick={ PlaceOrder }
+              >
+                Place your order
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   )
