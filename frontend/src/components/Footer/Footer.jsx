6bcd50badd5d328d7d947d9c56678ce4e2256fd7
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-05-22 21:54:12 -0400

    Updated footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return <div  className='bg-zinc-800 text-white px-8 py-2 items-center'>
-    <h1 className='text-xl font-semibold text-center'>&copy; 2025, Made By Grizzley Team</h1>
+    <h1 className='text-xl font-semibold text-center'>&copy; 2025, Made By Jeshan </h1>
     </div>
 };
 export default Footer;
